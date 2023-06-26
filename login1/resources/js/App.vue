e8c0996452273f485198676b9b7c8d09c5ce0fb1
emilioff8822 <emilioff@hotmail.it>
2023-06-26 20:24:05 +0200

    print date

--- login1/resources/js/App.vue
+++ login1/resources/js/App.vue
@@ -1,19 +1,46 @@
-
 <script>
-export default {
-    name: 'home'
+import { store } from './store/store.js';
+import axios from 'axios';
 
+export default {
+    name: 'home',
+    data() {
+        return {
+            posts: []
+        };
+    },
+    methods: {
+        getApi() {
+            console.log(store.apiUrl);
+            //faccio la chiamata axios
+            axios.get(store.apiUrl + 'posts')
+                .then(results => {
+                    this.posts = results.data;
+                    console.log(results.data);
+                });
+        },
+        formatData(dateString) {
+            const d = new Date(dateString);
+            return d.toLocaleDateString();
+        }
+    },
+    mounted() {
+        this.getApi();
+    }
 }
 </script>
 
-
 <template>
+    <div class="container">
+        <h1>Elenco Post</h1>
 
-<h1>Home vue</h1>
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+                <span>{{ post.title }}</span> - <span>{{ formatData(post.date) }}</span>
+            </li>
+        </ul>
 
+    </div>
 </template>
 
-
-<style>
-
-</style>
+<style></style>
